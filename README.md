cd4d5f22b9f439bf26f7ad22100533f9fc95a71a
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-01-13 15:44:46 -0800

    add readme.md and changes on index.html, styles.css and scripts.js

---
+++ README.md
@@ -0,0 +1,27 @@
+# _Track Suggester_
+
+#### _A quiz to find the right programming language for you, 01,13,2017_
+
+#### By _**Stella Huayhuaca**_
+
+![screenshot](img/language3.png)
+
+## Description
+
+_This is a quick test to figure out what programming language to study at Epicodus._
+
+## Setup/Installation Requirements
+
+* _To download and install this program, just clone the repository. Something like this:_
+$ git clone https://github.com/uturunku1/track-suggester.git
+* _Also you can take the quiz going directly to our site or gh-pages: https://uturunku1.github.io/track-suggester/
+
+## Support and contact details
+
+_Dear user: if you run into any issues or have questions or suggestion, please feel free to contact me or to make a contribution to the code._
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap and Jquery._
+
+Copyright (c) 2017 **_Stella Huayhuaca_**
--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
     var workplace= $("input:radio[name=workplace]:checked").val();
+    //Path towards Ruby or PHP.
     if (workplace==="startup") {
       $(".question1").hide();
       $(".question2").show();
@@ -9,17 +10,83 @@ $(document).ready(function() {
         event.preventDefault();
         var rubyvsphp= $("input:radio[name=rubyvsphp]:checked").val();
         if (rubyvsphp) {
-          $(".language").text(rubyvsphp);
-          $("form#quiz").hide();
-          $("form#results").show();
+          $(".question2").hide();
+          $(".lastquestion").show();
+          $("form#quiz").submit(function(event) {
+            event.preventDefault();
+            var all =$("input:radio[name=all]:checked").val();
+            if (all!= "none" && rubyvsphp!=all) {
+              $(".language2").text(all);
+              $(".language").text(rubyvsphp);
+              $("form#quiz").hide();
+              $("p.maybe").show();
+              $("form#results").show();
+            }else {
+              $(".language").text(rubyvsphp);
+              $("form#quiz").hide();
+              $("form#results").show();
+            }
+          });
         }
       });
+    //Path towards c#, java or PHP
     } else if (workplace=="company") {
       $(".question1").hide();
-      $(".question3").show();
-    }
+      $(".question2b").show();
+      $("form#quiz").submit(function(event) {
+        event.preventDefault();
+        var cjavaphp= $("input:radio[name=cjavaphp]:checked").val();
+        if (cjavaphp==="PHP") {
+          $(".question2b").hide();
+          $(".lastquestion").show();
+          $("form#quiz").submit(function(event) {
+            event.preventDefault();
+            var all =$("input:radio[name=all]:checked").val();
+            if (all!= "none" && cjavaphp!=all) {
+              $(".language2").text(all);
+              $(".language").text(cjavaphp);
+              $("form#quiz").hide();
+              $("p.maybe").show();
+              $("form#results").show();
+            }else {
+              $(".language").text(cjavaphp);
+              $("form#quiz").hide();
+              $("form#results").show();
+            }
+          });
+      //Path towards C# or Java
+        }else if (cjavaphp==="cjava") {
+          $(".question2b").hide();
+          $(".question3").show();
+          $("form#quiz").submit(function(event) {
+            var cjava= $("input:radio[name=cjava]:checked").val();
+            if (cjava) {
+              $(".question3").hide();
+              $(".lastquestion").show();
+              $("form#quiz").submit(function(event) {
+                event.preventDefault();
+                var all =$("input:radio[name=all]:checked").val();
+                if (all!= "none" && cjava!=all) {
+                  $(".language2").text(all);
+                  $(".language").text(cjava);
+                  $("form#quiz").hide();
+                  $("p.maybe").show();
+                  $("form#results").show();
+                }else {
+                  $(".language").text(cjava);
+                  $("form#quiz").hide();
+                  $("form#results").show();
+                }
+              });
 
+            }
+          });
+        }
+      });
+    }
   });
+
+
   $("form#results").submit(function(event) {
     event.preventDefault();
     var name= $("input#name").val();
@@ -29,5 +96,4 @@ $(document).ready(function() {
     $("form#results").hide();
     $(".alert.alert-success").show();
   });
-
 });
